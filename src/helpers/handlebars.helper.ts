daf4ca9759ec05e1685159460767786d7028b288
muhammadpauzi <muhammadpauzi200@gmail.com>
2021-12-01 20:57:02 +0700

    fix: updatedAt of class' data

--- src/helpers/handlebars.helper.ts
+++ src/helpers/handlebars.helper.ts
@@ -1,10 +1,13 @@
 import moment from 'moment';
 
 export default {
-    dateFormatFromNow: function (date: any) {
+    dateFormatFromNow: function (date: any): string {
         return moment(date).fromNow();
     },
-    dateFormatDefault: function (date: any, format: string) {
+    dateFormatDefault: function (date: any, format: string): string {
         return moment(date).format('lll');
+    },
+    isSame: function (value1: any, value2: any): boolean {
+        return value1 == value2;
     }
 }
